04819525c9205bdbebe82a906188327b5d376daa
Collins Kandie <collinskandie1@gmail.com>
2021-11-01 22:04:25 +0300

    Added new javascript line

--- js/eventhandlers.js
+++ js/eventhandlers.js
@@ -1,7 +1,47 @@
-var wrd = document.getElementById("word");
-wrd.onclick = function() {
-    window.alert('Collins')
+/**
+ * show/hide div
+ */
+const toggleBtn = document.querySelector('#toggleBtn');
+const divList = document.querySelector('.listHolder');
+
+//event listener
+toggleBtn.addEventListener('click', () => {
+    if (divList.style.display === 'none') {
+        divList.style.display = 'block';
+        toggleBtn.innerHTML = 'Hide List';
+    } else {
+        divList.style.display = 'none';
+        toggleBtn.innerHTML = 'Show List';
+    }
+
+});
+
+//add items
+const addInput = document.querySelector('#addInput');
+const addBtn = document.querySelector('#addBtn');
+
+//function
+function addList() {
+    if (addInput.value === '') {
+        alert('List item cannot be empty');
+    } else {
+        const ul = divList.querySelector('ul');
+        const li = document.createElement('li');
+        li.innerHTML = addInput.value;
+        addInput.value = '';
+        ul.appendChild(li);
+    }
+
 }
-document.getElementById('myBtn').addEventListener('clickMe', function() {
-    alert("Hi collins");
+//event listener
+addBtn.addEventListener('click', () => {
+    addList();
+});
+// Enter key
+addInput.addEventListener('keyup', (event) => {
+    if (event.which === 13) {
+        addList();
+    }
+
+
 });
