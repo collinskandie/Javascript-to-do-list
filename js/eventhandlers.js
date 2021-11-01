471383a2573bd4367493e96d68a0af20c4be7ce8
Collins Kandie <collinskandie1@gmail.com>
2021-11-01 19:31:51 +0300

    Learning javascript

---
+++ js/eventhandlers.js
@@ -0,0 +1,7 @@
+var wrd = document.getElementById("word");
+wrd.onclick = function() {
+    window.alert('Collins')
+}
+document.getElementById('myBtn').addEventListener('clickMe', function() {
+    alert("Hi collins");
+});
